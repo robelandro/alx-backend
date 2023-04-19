7847d5ed6475b83d0ea1446f43ede97d7130c835
robelpop <robelpopandro@gmail.com>
2023-04-19 17:51:41 -0200

    Fix: Last frequently used caching task

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,42 +1,54 @@
-#!/usr/bin/python3
-""" LFUCache module
+#!/usr/bin/env python3
 """
+LFUCache module
+"""
+from collections import defaultdict
 from base_caching import BaseCaching
 
 
 class LFUCache(BaseCaching):
-    """
-    LFUCache- Last Freq Used Cache
+    """LFUCache Latest Frequency Used caching system
     """
 
-    def __init__(self):
-        """
-        init
+    def __init__(self) -> None:
+        """Constructor
         """
         super().__init__()
-        self.keys = {}
+        self.keys = []
+        self.frequency = defaultdict(lambda: 0)
 
     def put(self, key, item):
+        """Saves the item to the cache with the given key
+
+        :param key: key to save the item
+        :param item: item to save
         """
-        put method
-        """
-        if key and item:
-            if key in self.keys:
-                self.keys[key] += 1
-            else:
-                self.keys[key] = 1
-            self.cache_data[key] = item
-            if len(self.keys) > BaseCaching.MAX_ITEMS:
-                min_key = min(self.keys, key=self.keys.get)
-                del self.cache_data[min_key]
-                del self.keys[min_key]
-                print("DISCARD: {}".format(min_key))
+        if key is None or item is None:
+            return
+
+        if len(self.keys) == self.MAX_ITEMS and key not in self.keys:
+            min_freq_key = min(self.frequency, key=self.frequency.get)
+            self.keys.remove(min_freq_key)
+            self.frequency.pop(min_freq_key)
+            self.cache_data.pop(min_freq_key)
+            print(f'DISCARD: {min_freq_key}')
+
+        if key in self.keys:
+            self.frequency[key] += 1
+        else:
+            self.keys.append(key)
+            self.frequency[key] = 1
+
+        self.cache_data[key] = item
 
     def get(self, key):
+        """ Returns the value in self.cache_data linked to key
+
+        :param key: key to search for
         """
-        get method
-        """
-        if key in self.cache_data:
-            self.keys[key] += 1
-            return self.cache_data[key]
-        return None
+        value = self.cache_data.get(key)
+        if value is not None:
+            self.frequency[key] += 1
+            self.keys.remove(key)
+            self.keys.append(key)
+        return value
