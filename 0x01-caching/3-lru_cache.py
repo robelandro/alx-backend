b54232acf67c567a27e969b2033b354872a6cf0a
robelpop <robelpopandro@gmail.com>
2023-04-19 17:10:04 -0200

    Solve: Last recently used caching task

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" LRUCache module
+"""
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    LRUCache- last recently used caching system
+    """
+
+    def __init__(self):
+        """
+        init
+        """
+        super().__init__()
+        self.keys = []
+
+    def put(self, key, item):
+        """
+        put method
+        """
+        if key and item:
+            if key in self.keys:
+                self.keys.remove(key)
+            self.keys.append(key)
+            self.cache_data[key] = item
+            if len(self.keys) > BaseCaching.MAX_ITEMS:
+                last = self.keys.pop(0)
+                del self.cache_data[last]
+                print("DISCARD: {}".format(last))
+
+    def get(self, key):
+        """
+        get method
+        """
+        if key in self.cache_data:
+            self.keys.remove(key)
+            self.keys.append(key)
+            return self.cache_data[key]
+        return None
