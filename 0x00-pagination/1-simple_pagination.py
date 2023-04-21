fbe2c9697b685797b93a064cc12db70f40d575ed
robelpop <robelpopandro@gmail.com>
2023-04-21 16:37:16 -0200

    Solve: function that return end and start

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""Simple pagination"""
+
+import csv
+import math
+from typing import List
+
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Get a page of the dataset
+        """
+        assert type(page) == int and page > 0
+        assert type(page_size) == int and page_size > 0
+        start, end = index_range(page, page_size)
+        if start >= len(self.dataset()):
+            return []
+        return self.dataset()[start:end]
