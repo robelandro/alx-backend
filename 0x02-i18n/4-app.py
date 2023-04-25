d961b1ca8ae28723b1c4c9b061bc8f558a38a142
robelpop <robelpopandro@gmail.com>
2023-04-25 18:44:47 -0200

    Fix: change name 4-index

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -31,7 +31,7 @@ def get_locale():
 @app.route('/')
 def root_path():
     """Method for root route."""
-    return render_template("3-index.html")
+    return render_template("4-index.html")
 
 
 if __name__ == "__main__":
