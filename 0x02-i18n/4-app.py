562e9a5e2c21f7b0493598337b9d96b1db7c8ba7
robelpop <robelpopandro@gmail.com>
2023-04-25 18:43:18 -0200

    Solve: 4. Force locale with URL parameter And Fix: remove autoescape

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Module for 4. Force locale with URL parameter task.
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """Class for config."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+    
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """Method for get locale."""
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.route('/')
+def root_path():
+    """Method for root route."""
+    return render_template("3-index.html")
+
+
+if __name__ == "__main__":
+    app.run(port=5000)
