3e9cd2156591b5cc73d8bcb832beb87f3bfaf098
robelpop <robelpopandro@gmail.com>
2023-04-25 16:38:31 -0200

    Solve: Basic Flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module for 0. Basic Flask app task.
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Render index.html"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port=5000)
